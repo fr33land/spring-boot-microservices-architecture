5f966dd2243f9e85e7b3a55c9fb876d5ddc8a39a
fr33land <rrokass@gmail.com>
2018-05-20 23:20:53 +0300

    Updated some services to spring boot version 2 and added spring cloud config  server

--- UsersService/src/main/java/lt/freeland/users/config/DataSourceConfig.java
+++ UsersService/src/main/java/lt/freeland/users/config/DataSourceConfig.java
@@ -1,8 +1,8 @@
 package lt.freeland.users.config;
 
 import javax.sql.DataSource;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
