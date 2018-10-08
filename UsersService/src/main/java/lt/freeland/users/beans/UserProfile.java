73934c4ad80cc73907ca85f6bef0459eb54ee671
fr33land <rrokass@gmail.com>
2018-10-08 22:49:09 +0300

    changed table column names

--- UsersService/src/main/java/lt/freeland/users/beans/UserProfile.java
+++ UsersService/src/main/java/lt/freeland/users/beans/UserProfile.java
@@ -34,10 +34,10 @@ public class UserProfile implements Serializable {
     @Column(name = "user_id")
     private Long userId;
     
-    @Column(name = "firstName")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "lastName")
+    @Column(name = "last_name")
     private String lastName;
     
     @Column(name = "birthday")
