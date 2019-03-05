738c6089bb420fce6b20bece5a679b805b9bb54a
fr33land <rrokass@gmail.com>
2019-03-05 20:55:16 +0200

    Flyway db migration project

---
+++ DbMigration/src/main/java/lt/freeland/DbMigration/DbMigrationApplication.java
@@ -0,0 +1,13 @@
+package lt.freeland.DbMigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbMigrationApplication.class, args);
+	}
+
+}
