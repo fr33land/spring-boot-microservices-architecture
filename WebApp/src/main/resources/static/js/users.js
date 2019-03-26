9dbacb9cb9886b585317b62e113d6669c0c63229
fr33land <rrokass@gmail.com>
2019-03-26 22:13:58 +0200

    Update users.js
    added server side sorting and paging

--- WebApp/src/main/resources/static/js/users.js
+++ WebApp/src/main/resources/static/js/users.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
             'url': '/dashboard/admin/users/list',
             'type': 'POST',
             'data': function (d) {
+                d.columns.splice(5, 1);
                 return JSON.stringify(d);
             }
         },
@@ -13,32 +14,30 @@ $(document).ready(function () {
             {'data': 'user.username'},
             {'data': 'user.email'},
             {'data': 'user.status'},
-            {'data': 'createdDate'},
-            {'data': null}
-        ],
-        'order': [[1, 'asc']],
-        'columnDefs': [
-            {targets: 4, render: function (data) { 
+            {'data': 'user.createdDate', render: function (data) {
                     return moment(data).format('YYYY-MM-DD HH:mm:ss');
                 }
             },
-            {targets: 5, render: function (data, type, row, meta) { 
-                    return data =   '<a class="pencil" href=/dashboard/admin/users/edit/' + row.userId + '><i class="fas fa-pencil-alt"></i></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'+
-                                    '<a class="delete-user trash" data-id=' + row.userId + ' href=#><i class="fas fa-trash-alt"></i></a>';
+            {'data': null, 'orderable': false, 'searchable': false, render: function (data, type, row, meta) {
+                    return data = '<a class="pencil" href=/dashboard/admin/users/edit/' + row.userId + '><i class="fas fa-pencil-alt"></i></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp' +
+                            '<a class="delete-user trash" data-id=' + row.userId + ' href=#><i class="fas fa-trash-alt"></i></a>';
                 }
             }
-        ]
+        ],
+        'serverSide': true,
+        'order': [[1, 'asc']]
     });
-    
-    $('#table-users tbody').on('click', '.delete-user', function() {
+
+    $('#table-users tbody').on('click', '.delete-user', function () {
         var ctx = $(this);
         $.ajax({
             url: '/dashboard/admin/users/delete/' + ctx.data('id'),
             type: 'DELETE',
-            success: function(result) {
-                ctx.closest("tr").fadeOut(500, function(){ 
-                    $(this).remove();                    
-                }); ;
+            success: function (result) {
+                ctx.closest("tr").fadeOut(500, function () {
+                    $(this).remove();
+                });
+                ;
             }
         });
     });
